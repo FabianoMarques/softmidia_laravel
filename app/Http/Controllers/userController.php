3e1427262c0f601805dc41d4ef3edbaa1d14e69f
Fabiano Marques <39709425+FabianoMarques@users.noreply.github.com>
2020-08-10 18:31:17 -0300

    Add files via upload

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace App\Http\Controllers;
+    use Illuminate\Http\Request;
+
+    class userController extends Controller{
+
+        public function teste(){
+
+            echo "Fabiano marques";
+
+        }
+
+
+    }
+
+
+
+
+
+?>
